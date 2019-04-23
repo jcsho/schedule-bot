56c0f351149c7a20d0452ad13f1708b00a4a8317
Justin Ho <justinhodev@gmail.com>
2019-04-22 23:37:53 -0700

    Create Database Class
    connect to remote PostgreSQL db
    add logging for exceptions
    add create user table

--- src/main/java/schedulebot/Main.java
+++ src/main/java/schedulebot/Main.java
@@ -1,42 +1,8 @@
 package schedulebot;
 
-import net.dv8tion.jda.core.AccountType;
-import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.core.hooks.ListenerAdapter;
+public class Main {
 
-public class Main extends ListenerAdapter {
-
-    /**
-     * @throws Exception
-     */
-    public static void main() throws Exception {
-        try {
-            // read token property from bot-token.properties file
-            String token = new PropertiesHandler("bot-token").get("token");
-            // create new JDA instance
-            JDABuilder builder = new JDABuilder(AccountType.BOT);
-            builder.setToken(token);
-            // add !ping example
-            builder.addEventListener(new Main());
-            builder.build();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Setup example for !ping command
-     * @param event
-     */
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-        if (event.getAuthor().isBot()) return;
-
-        System.out.println("We receieved a message from " + event.getAuthor().getName() + ": " + event.getMessage().getContentDisplay());
-
-        if (event.getMessage().getContentRaw().equals("!ping")) {
-            event.getChannel().sendMessage("Pong!").queue();
-        }
+    public static void main(String[] args) {
+        Database db = new Database();
     }
 }
